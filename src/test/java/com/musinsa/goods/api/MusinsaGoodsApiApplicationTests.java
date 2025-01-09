db1eb7240a447e9de3e18e89c83f15662106185a
tgkang <ktaegoo@nate.com>
2025-01-09 14:54:13 +0900

    initial commit

---
+++ src/test/java/com/musinsa/goods/api/MusinsaGoodsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.goods.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaGoodsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
